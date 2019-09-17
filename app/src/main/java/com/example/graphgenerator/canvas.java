861419e0ce0409e2096c47217b4bb1562c39e0d8
kewalg <kewalgulve007@gmail.com>
2019-09-17 17:55:42 -0400

    UI Changes

--- app/src/main/java/com/example/graphgenerator/canvas.java
+++ app/src/main/java/com/example/graphgenerator/canvas.java
@@ -10,6 +10,5 @@ public class canvas extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_canvas);
-
     }
 }
--- app/src/main/java/com/example/graphgenerator/graphs.java
+++ app/src/main/java/com/example/graphgenerator/graphs.java
@@ -2,40 +2,69 @@ package com.example.graphgenerator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
-
 public class graphs extends AppCompatActivity {
-    LineGraphSeries<DataPoint> series;
+    LineGraphSeries<DataPoint> series,series1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_canvas);
+        setContentView(R.layout.activity_graphs);
 
-        Button sin_btn = (Button) findViewById(R.id.sin_submit);
-        EditText sin_edttxt = (EditText)findViewById(R.id.sin_edtext);
 
-        sin_btn.setOnClickListener(new View.OnClickListener() {
+        final GraphView graph = (GraphView) findViewById(R.id.graphviewtest);
+        Button btn_submit = (Button) findViewById(R.id.sin_submit);
+        Button btn_submit2 = (Button)findViewById(R.id.sin_submit1);
+
+        series = new LineGraphSeries<DataPoint>();
+        series1 = new LineGraphSeries<DataPoint>();
+
+
+        btn_submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                double x,y;
-                x=0.0;
-                GraphView graph = (GraphView)findViewById(R.id.graphviewtest);
-                series = new LineGraphSeries<DataPoint>();
-                for (int i =0;i<250;i++){
-                    x = x+0.1;
-                    y = Math.sin(x);
-                    series.appendData(new DataPoint(x,y), true, 250);
+
+                graph.getViewport().setXAxisBoundsManual(true);
+                graph.getViewport().setMinX(-10);
+                graph.getViewport().setMaxX(10);
+                graph.getViewport().setScalable(true);
+                graph.getViewport().setScrollable(true);
+                graph.getViewport().setYAxisBoundsManual(true);
+                graph.getViewport().setScalableY(true);
+                graph.getViewport().setScrollableY(true);
+                double x= -10,y;
+
+                for (int i = 0; i < 200; i++) {
+                    x = x + 0.1;
+                    y = 0.5 * (Math.sin(x));
+                    series.appendData(new DataPoint(x, y), true, 200);
                 }
                 graph.addSeries(series);
+
+            }
+        });
+
+
+        btn_submit2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                series1.setColor(Color.BLACK);
+                double v= -10,z;
+                for (int j = 0; j < 200; j++) {
+                    v = v + 0.1;
+                    z = 3 * (Math.sin(v));
+                    series.appendData(new DataPoint(v, z), true, 200);
+                }
+                graph.addSeries(series1);
             }
         });
 
