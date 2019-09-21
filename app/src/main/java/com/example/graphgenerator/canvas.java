e8356a7cd2ad273ac7b24551160e14471a820383
kewalg <kewalgulve007@gmail.com>
2019-09-20 21:07:53 -0400

    canvas implemented

--- app/src/main/java/com/example/graphgenerator/canvas.java
+++ app/src/main/java/com/example/graphgenerator/canvas.java
@@ -2,13 +2,37 @@ package com.example.graphgenerator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 public class canvas extends AppCompatActivity {
+    DrawView drawView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_canvas);
+
+        drawView = new DrawView(this);
+        drawView.setBackgroundColor(Color.WHITE);
+        setContentView(drawView);
+        drawView.requestFocus();
+        Button btn = findViewById(R.id.btn_canvas_clear);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DrawView.points.clear();
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/graphgenerator/coordi_tracker.java
+++ app/src/main/java/com/example/graphgenerator/coordi_tracker.java
@@ -9,22 +9,78 @@ import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.w3c.dom.Text;
+
 public class coordi_tracker extends AppCompatActivity {
-
-
-    float dX;
-    float dY;
+    float dX, dY;
     int lastAction;
-    View.OnTouchListener touchListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coordi_tracker);
+        View.OnTouchListener touchListener;
+
+        touchListener = new View.OnTouchListener() {
+            float dx = 0, dy = 0, x = 0, y = 0;
+            RelativeLayout.LayoutParams parms;
+            LinearLayout.LayoutParams par;
+            View view = findViewById(R.id.circle);
+
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+                Button btn = findViewById(R.id.coordi_btn);
+                final TextView txt = findViewById(R.id.coordi_txt);
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN: {
+                        parms = (RelativeLayout.LayoutParams) view.getLayoutParams();
+                        //par = (LinearLayout.LayoutParams) getWindow().findViewById(Window.ID_ANDROID_CONTENT).getLayoutParams();
+                        dx = event.getRawX() - parms.leftMargin;
+                        dy = event.getRawY() - parms.topMargin;
+                        Log.i("Position 1", "position 1 is:" + dx + " , " + dy);
+                    }
+                    break;
+                    case MotionEvent.ACTION_MOVE: {
+                        x = event.getRawX();
+                        y = event.getRawY();
+                        parms.leftMargin = (int) (x - dx);
+                        parms.topMargin = (int) (y - dy);
+                        view.setLayoutParams(parms);
+                        btn.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                txt.setText("Co-ordinates are: " + dx + " , " + dy);
+                            }
+                        });
+                    }
+                    break;
+                    case MotionEvent.ACTION_UP: {
+                    }
+                    break;
+                }
+                return true;
+            }
+        };
+        final View dragView = findViewById(R.id.circle);
+        dragView.setOnTouchListener(touchListener);
+    }
+}
+
+
+
+/*      final View view = findViewById(R.id.circle);
+        final Button btn = findViewById(R.id.coordi_btn);
+        final TextView txt = findViewById(R.id.coordi_txt);
+        View.OnTouchListener touchListener;
 
         touchListener = new View.OnTouchListener() {
             @Override
@@ -35,12 +91,21 @@ public class coordi_tracker extends AppCompatActivity {
                         dY = view.getY() - event.getRawY();
                         lastAction = MotionEvent.ACTION_DOWN;
                         break;
+
+
                     case MotionEvent.ACTION_MOVE:
                         view.setY(event.getRawY() + dY);
                         view.setX(event.getRawX() + dX);
-
                         lastAction = MotionEvent.ACTION_MOVE;
+                        btn.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                txt.setText(dX + " , " + dY);
+                            }
+                        });
                         break;
+
+
                     case MotionEvent.ACTION_UP:
                         if (lastAction == MotionEvent.ACTION_DOWN) {
                             Toast.makeText(coordi_tracker.this, "Clicked!", Toast.LENGTH_SHORT).show();
@@ -55,8 +120,32 @@ public class coordi_tracker extends AppCompatActivity {
         };
         final View dragView = findViewById(R.id.circle);
         dragView.setOnTouchListener(touchListener);
-    }
-}
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
